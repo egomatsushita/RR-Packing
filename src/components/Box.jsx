99d46bc1c28d692c5971b3b670e60f5b237965a2
egomatsushita <eduardo.go.rj@gmail.com>
2018-04-08 16:27:11 -0400

    Handle sum of items' weight in the box

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 
-export const Box = ({box, items}) => {
+export const Box = ({box, items, totalWeight}) => {
+  let ItemsBox;
   const itemsInBox = items.filter(item => item.box_id === box._id);
+  let boxName = box.name;
+  boxName = boxName.charAt(0).toUpperCase() + boxName.slice(1);
 
-  const ItemsBox = () => {
-    if (itemsInBox) {
-      return (
-        <ul className="ul-items-in-box">
-          {itemsInBox.map(item => (
+  if (itemsInBox) {
+    ItemsBox = () => (
+      <ul className="ul-items-in-box">
+        {itemsInBox.map(item => {
+          return (
             <li key={item._id}>
               <p><span>x</span>&nbsp;{item.name}</p> 
               <p>{item.weight}</p>
-            </li>))}
-        </ul>
-      );
-    }
-    return null;
+            </li>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    ItemsBox = null;
   }
-
-  let boxName = box.name;
-  boxName = boxName.charAt(0).toUpperCase() + boxName.slice(1);
 
   return (
     <li className="li-boxes">
       <p>{boxName}</p>
       <ItemsBox />
-      <p>t.w. 0/{box.total_allowed_weight}</p>
+      <p>t.w. {totalWeight}/{box.total_allowed_weight}</p>
     </li>
   );
 }
--- src/components/BoxList.jsx
+++ src/components/BoxList.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { Box } from './Box';
 
 export const BoxList = ({boxes, items}) => {
+  let ItemsBox;
+
   return (
     <section className="boxes">
       <p className="p-title">Boxes:</p>
       <ul className="ul-section ul-items-boxes">
-        {boxes.map(box =>
-          <Box key={box._id} box={box} items={items}/>
-        )}
+        {boxes.map(box => {
+          const totalWeight = 
+            items
+              .filter(item => item.box_id === box._id)
+              .reduce((sum, item) => sum + parseInt(item.weight), 0);
+          
+          return <Box key={box._id} box={box} items={items} totalWeight={totalWeight}/>
+        })}
       </ul>
     </section>
   );
 }
 
 export default BoxList;
-
